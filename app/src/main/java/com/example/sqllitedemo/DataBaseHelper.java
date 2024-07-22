5143dfe2080e493f675ff093b570f938e1b8c906
tomas <tomasmcgrath21@gmail.com>
2024-07-22 12:38:08 +0100

    add database contsructor for sqliteopenhelper

---
+++ app/src/main/java/com/example/sqllitedemo/DataBaseHelper.java
@@ -0,0 +1,26 @@
+package com.example.sqllitedemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "customer.db", null, 1);
+    }
+
+    //this is called the first time a database is accessed . There should be code in here to create a new database
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    //this is called if the database version changes. It prevents previous users apps from breaking when you change the database design. Updates Schema.
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
