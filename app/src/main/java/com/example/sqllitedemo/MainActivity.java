4fe7a0d6f943ac9c76be213945dbe3af90e964e4
tomas <tomasmcgrath21@gmail.com>
2024-07-22 12:03:16 +0100

    Add set on click listereners for buttons view all and add

--- app/src/main/java/com/example/sqllitedemo/MainActivity.java
+++ app/src/main/java/com/example/sqllitedemo/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.sqllitedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //references to buttons and other controls on the layout
+    Button btn_add, btn_viewAll;
+    EditText et_name, et_age;
+    Switch sw_activeCustomer;
+    ListView lv_customerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        et_age = findViewById(R.id.et_age) ;
+        et_name = findViewById(R.id.et_name);
+        sw_activeCustomer = findViewById(R.id.sw_active);
+        lv_customerList = findViewById(R.id.lv_customerList);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Add button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "View all button", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
