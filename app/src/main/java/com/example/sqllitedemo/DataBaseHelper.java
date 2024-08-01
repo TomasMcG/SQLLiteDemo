ec38a34c8c2237bf5708a462dcfb83dc7121a10a
tomas <tomasmcgrath21@gmail.com>
2024-08-01 20:17:22 +0100

    add method to get all customers from the database

--- app/src/main/java/com/example/sqllitedemo/DataBaseHelper.java
+++ app/src/main/java/com/example/sqllitedemo/DataBaseHelper.java
@@ -2,10 +2,14 @@ package com.example.sqllitedemo;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
@@ -48,6 +52,42 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         else  {
             return true;
         }
+      }
+
+      public List<CustomerModel> getEveryone(){
+        List<CustomerModel> returnList = new ArrayList<>();
+        //need to get data from the database
+            String queryString = "Select * From " + CUSTOMER_TABLE; //use the customer table constant
+          SQLiteDatabase db = this.getReadableDatabase(); //this is a reference only need readable , not writable, writable locks data file so other processes cant access it, bottleneck
+          Cursor cursor = db.rawQuery(queryString,null);//rawQuery has cursor return type, introduce local variable with lightbul, Cursor is the result set in SQLite,complex array of items
+          //rawquery neesd sql string which is the sql statement and seleciton Args, ?s in statement that can be replaced, we will use null
+
+          if(cursor.moveToFirst())  //if firest result in result set is true and exists, thenm there was results
+          {
+              //if results loop throught the cursor(result set) and create new customer objects. Put them into the reutrn list, new customer object for each row
+              do{
+                  //local variable of type to come from database
+                  int customerID = cursor.getInt(0); //position 0
+                  String customerName = cursor.getString(1);
+                  int customerAge = cursor.getInt(2);
+                  boolean customerActive = cursor.getInt(3) == 1 ? true: false; //no booleans, only ints of 0 or 1, take int and convert ot boolean , turnary operator, if position 3 equuals 1 then result is true, other false
+
+                  CustomerModel newCustomer = new CustomerModel(customerID,customerName,customerAge,customerActive);
+                  returnList.add(newCustomer);
+              }
+              while(cursor.moveToFirst()); //do it while there is new lines
+
+          }
+          else{
+              //failure, do not add anything to the list , null list
+
+          }
+
+          //need to close conneciton to cursor and the db after yourself when done
+          cursor.close();
+          db.close();
+          return returnList;
+
       }
 
     }
